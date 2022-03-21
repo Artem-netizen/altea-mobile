c694b1decff4b8ebf207dd12b24eb1f2a2d0eb99
Artem-netizen <82020900+Artem-netizen@users.noreply.github.com>
2022-03-22 05:08:08 +0800

    altea

---
+++ js/inform-slider.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.block__inform-wrapper', {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+});
